53998ca4763220784c991c149377bda02420f501
Jason Ji <jimanproductions@gmail.com>
2023-01-26 10:42:43 +0100

    different pages works

--- newServerEjs.js
+++ newServerEjs.js
@@ -4,11 +4,22 @@ const app = express();
 
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// app.set("views", path.join(__dirname, "views"));
 
-app.listen(3000)
 
 app.get('/', (req, res) => {
     res.send('<h1>It works bruh</h1>');
 });
 
+
+app.get('/index', (req, res)=> {
+    res.render('index');
+})
+
+app.get('/deeznuts', (req, res)=> {
+    res.send('<h1>what what yeah!</h1>');
+})
+
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
